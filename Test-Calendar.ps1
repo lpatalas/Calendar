dd306a4cd24bb8848a386ff00ef8bea4aa539c21
Łukasz Patalas <luke.patalas@gmail.com>
2014-02-08 12:06:47 +0100

    Add test related to marking subsequent days

--- Test-Calendar.ps1
+++ Test-Calendar.ps1
@@ -8,6 +8,7 @@ $commands = @(
     'Show-Calendar (Get-Date).AddMonths(3)'
     'Get-Date | Show-Calendar'
     '1..5 | %{ Get-Date -Year 2014 -Month ($_ * 2) } | Show-Calendar'
+    '7..22 | %{ Get-Date -Year 2014 -Month 2 -Day $_ } | Show-Calendar'
 )
 
 foreach ($command in $commands) {
